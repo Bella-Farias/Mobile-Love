0dbb164f1589f28d6fbc0dd7e9acf502f46fa8a5
Isabella Farias <belllafarias99@gmail.com>
2024-09-04 15:11:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const listItems = document.querySelectorAll("nav ul li");
 
 const clock = document.querySelector("#clock");
 
+const batteryLevelElement = document.querySelector("#batteryLevel");
+
 listItems.forEach((item, idx) => {
     item.addEventListener("click", () => {
         hideAllContent();
@@ -33,3 +35,24 @@ function time(){
 }
 
 setInterval(time, 1000);
+
+function updateBatteryInfo(battery) {
+    let level = Math.floor(battery.level * 100) + "%";
+    batteryLevelElement.textContent = `${level}`;
+}
+
+function monitorBattery() {
+    navigator.getBattery().then(function(battery) {
+        
+        updateBatteryInfo(battery);
+        
+        battery.addEventListener('levelchange', () => updateBatteryInfo(battery));
+        
+        battery.addEventListener('chargingchange', () => {
+            let chargingStatus = battery.charging ? "Charging" : "Not Charging";
+            batteryLevelElement.textContent += ` (${chargingStatus})`;
+        });
+    });
+}
+
+monitorBattery();
